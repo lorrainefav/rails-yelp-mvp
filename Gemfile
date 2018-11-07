056d873c008ab59714ac8a7d5942bae2de75a55c
Lorraine Favreau <lorraine.favreau@gmail.com>
2018-11-07 17:31:46 +0100

    done

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 65 67 89 90',
+    category:  'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '07 76 55 77 98',
+    category:  'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '06 87 65 43 54',
+    category:  'japanese'
+  },
+  {
+    name:         'Chez Bibi',
+    address:      'rue hupette',
+    phone_number:  '06 76 89 98 67',
+    category:  'french'
+  },
+  {
+    name:         'Pizza East',
+    address:      'avenue saint ouen',
+    phone_number:  '06 78 89 98 98',
+    category:  'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
